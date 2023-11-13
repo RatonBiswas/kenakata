c2ae9256ef440b3bb44cabef38537ce8b5b99580
Raton Biswas <raton35-2241@diu.edu.bd>
2023-11-13 23:25:01 +0600

    header & footer with custom bootstrap theme

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <>
+      <Header/>
+      <main className="py-3">
+        <Container>
+         <h1>Welcome to KineNao</h1>
+        </Container>
+      </main>
+      <Footer/>
+    </>
+  );
+};
+
+export default App;

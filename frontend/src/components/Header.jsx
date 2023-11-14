f5601470b66ad1e031f264e62202f9563a5793e2
Raton Biswas <raton35-2241@diu.edu.bd>
2023-11-14 12:51:49 +0600

    Implement React router

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,21 +1,30 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">KenaKataShop</Navbar.Brand>
+          <LinkContainer to="/"> 
+          {/* Navbar.Brand bebohar korle href use korte hobe but page reload nibe , ar tar jonno amader linkcontainer bebohar korte hobe its like Link */}
+            {/* <Navbar.Brand href='/'>KenaKataShop</Navbar.Brand> */} 
+            <Navbar.Brand>KenaKataShop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart">
-                <FaShoppingCart /> Cart
-              </Nav.Link>
-              <Nav.Link href="/login">
-                <FaUser /> Login
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart /> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <FaUser /> Login
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
